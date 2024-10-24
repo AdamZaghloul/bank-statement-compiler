b380f339ba038949e367291e1b5bc2c0edfff7d0
AdamZaghloul <adam.zaghloul95@gmail.com>
2024-10-24 06:51:36 -0400

    Version 1 of completed project. Added all config files, and updated main.py with remaining functionality.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # bank-statement-compiler
 Takes bank statement csv files as inputs and compiles them into a single csv for analysis.
+
+##Setup Instructions
+
+1. Clone repo on environment with python installed
+2. Populate config/accounts.csv with information about the different bank accounts in format "account Unique ID, Account Name, Account statement format, Account default Factor" where:
+    Account Unique ID - a unique string that shows up in every line of releant data rows of an input statement. I use the last 4 digits of the account.
+    Account Name - A name to be printed on the output e.g. "Savings Account"
+    Account Statement Format - A string of capital letters indicating the order of columns in the input statement:
+        N - Ignore/Not relevant
+        U - Unique ID (Required)
+        D - Date (Required)
+        I - Item Notes/Description (Required)
+        A - Amount (Required)
+    Account default factor - -1 or 1 depending on how values/amounts are reported in your input statement.
+3. Populate config/output_format.txt with your desired order of columns *(All required)*:
+    U - Unique ID
+    D - Date
+    I - Item Notes/Description
+    C - Category
+    S - Subcategory
+    A - Amount
+    T - Account
+    F - Filter flag
+4. Populate config/categories.csv with any automatic categorization rules in format "search string, category, subcategory, filter". See sample data in file where:
+    Search string is the string that should appear in any input row's item notes/description
+    Category, subcategory, filter are self-explanatory
+    NOTE a category of "DELETE" will indicate that the program will ignore rows. I use this to ignore things like internal transfers and plan charges with rebates.
+5. Populate copy_input_files_from.txt with the directory you plan to have your input files to be copied to the input/ directory. Useful if you're on WSL and don't want to manually move files. See sample data in file.
+6. Populate copy_output_to.txt with the directory you want your output file to be copied from the output/ directory. Useful if you're on WSL and don't want to manually move files. See sample data in file.
+
+##Running Instructions
+1. Copy your bank statement csv files to the input/ directory, or the directory from step 5 above if applicable
+2. From the project root directory, run "python3 main.py MM YYYY" in your terminal, where:
+    MM is the month you're analyzing (01 for Jan, 02 for Feb, etc.)
+    YYYY is the year you're analyzing
+3. Watch for confirmation message "Output ready at DIRECTORY"
+4. Retrieve your output csv from the DIRECTORY mentioned
+
+##Troubleshooting
+
+###Index out of range error
+1. Make sure that your input csv files are comma-delimited, not tab or anything else
+2. Make sure that your config/accounts.csv file input formats have all of the required characters listed above
+3. Make sure that your config/output_format.txt file has all of the required characters listed above
+
+###Output columns in wrong order
+Make sure that your config/output_format.txt file has all of the required characters listed above in your desired order.
+
+###Output column data not where expected
+Make sure that your config/accounts.csv file input formats have all of the required characters listed above in the correct order for your input file.
--- main.py
+++ main.py
@@ -13,11 +13,14 @@ def main():
     account_IDs = []
     account_names = []
     account_orders = []
+    account_default_factors = []
+
+    categories = []
 
     output_format = []
     output_columns = []
 
-    print(f"Preparing output for {datetime.date(year, month, 1).strftime('%B')}, {year}")
+    print(f"Preparing output for {datetime.date(year, month, 1).strftime('%B')} {year}")
 
     copy_input_files()
 
@@ -25,9 +28,15 @@ def main():
     if len(output_format) == 0:
         return
     
-    account_IDs, account_names, account_orders = get_accounts()
+    account_IDs, account_names, account_orders, account_default_factors = get_accounts()
     if len(account_IDs) == 0:
         return
+
+    categories = get_categories()
+
+    output_columns = process_input_files(output_columns, output_format, account_IDs, account_names, account_orders, month, year, categories, account_default_factors)
+
+    print_output_file(output_format, output_columns)
 
 def process_args(args):
     
@@ -65,7 +74,6 @@ def copy_input_files():
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-        #shutil.copytree(directory, 'input')
         for filename in os.listdir(directory):
             file_path = os.path.join(directory, filename)
             try:
@@ -94,29 +102,146 @@ def get_accounts():
     account_IDs = []
     account_names = []
     account_orders = []
+    account_default_factors = []
 
-    with open('config/accounts.csv', newline='') as csvfile:
-        accounts = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        if len(accounts) != 0:
-            for row in accounts:
-                account_IDs.append(row[0])
-                account_names.append(row[1])
-                temp_list = []
-                chars_so_far = ""
-                for char in row[2]:
-                    if(char in chars_so_far and char.upper() != "N"):
-                        print(f"ERROR: Invalid input format for {account_names.pop()} account in accounts.csv config file. All flags are allowed once and only once except for 'N'")
-                        return 0,0,0
+    with open('config/accounts.csv', newline='\n') as csvfile:
+        accounts = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in accounts:
+            account_IDs.append(row[0])
+            account_names.append(row[1])
+            temp_list = []
+            chars_so_far = ""
+            for char in row[2]:
+                if(char in chars_so_far and char.upper() != "N"):
+                    print(f"ERROR: Invalid input format for {account_names.pop()} account in accounts.csv config file. All flags are allowed once and only once except for 'N'")
+                    return 0,0,0
 
-                    chars_so_far = chars_so_far + char
-                    temp_list.append(char)
-                
-                account_orders.append(temp_list)
+                chars_so_far = chars_so_far + char
+                temp_list.append(char)
+            
+            account_orders.append(temp_list)
+            account_default_factors.append(int(row[3]))
+            if account_default_factors[-1] == 0:
+                account_default_factors[-1] = 1
     
     if len(account_IDs) != len(account_names) or len(account_names) != len(account_orders) or len(account_IDs) == 0 or len(account_names) == 0 or len(account_orders) == 0:
         print("ERROR: No accounts found in accounts.csv config file.")
         return 0,0,0
     
-    return account_IDs, account_names, account_orders
+    return account_IDs, account_names, account_orders, account_default_factors
+
+def get_categories():
+    categories = []
+
+    with open('config/categories.csv', newline='\n') as csvfile:
+        cats = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in cats:
+            inner_cat = []
+            inner_cat.append(row[0])
+            inner_cat.append(row[1])
+            inner_cat.append(row[2])
+            inner_cat.append(row[3])
+            categories.append(inner_cat)
+    
+    return categories
+
+def process_input_files(output_columns, output_format, account_IDs, account_names, account_orders, month, year, categories, account_default_factors):
+    for filename in os.listdir("input"):
+            file_path = os.path.join("input/", filename)
+            try:
+                if os.path.isfile(file_path) and filename[-4:] == ".csv":
+                    output_columns = process_single_input_file(file_path, output_columns, output_format, account_IDs, account_names, account_orders, month, year, categories, account_default_factors)
+                else:
+                    print(f"WARNING: Skipped non-CSV file or directory {file_path}")
+            except Exception as e:
+                print('WARNING: Failed to process %s. Reason: %s' % (file_path, e))
+    
+    return output_columns
+        
+def process_single_input_file(file_path, output_columns, output_format, account_IDs, account_names, account_orders, month, year, categories, account_default_factors):
+    account = None
+    account_dict = {}
+    
+    with open(file_path, newline='\n') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in rows:
+            if account is None:
+                for col in row:
+                    for i in range(0, len(account_IDs)):
+                        if account_IDs[i] in col:
+                            account = i
+                            for k in range(0,len(account_orders[account])):
+                                account_dict[account_orders[account][k]] = k
+            
+            if account is not None:
+                new_row = {}
+
+                #check if date and unique ID are valid using account_dict indices
+                if account_IDs[account] in row[account_dict["U"]] and int(row[account_dict["D"]][4:6]) == month and int(row[account_dict["D"]][0:4]) == year:
+                    #set new_row based on account_dict indices
+                    for i in range(0, len(output_format)-1):
+                        if output_format[i] in account_dict:
+                            if output_format[i] == "D":
+                                new_row[output_format[i]] = datetime.date(int(row[account_dict[output_format[i]]][0:4]), int(row[account_dict[output_format[i]]][4:6]), int(row[account_dict[output_format[i]]][6:])).isoformat()
+                            elif output_format[i] == "A":
+                                new_row[output_format[i]] = float(row[account_dict[output_format[i]]]) * account_default_factors[account]
+                            else:
+                                new_row[output_format[i]] = row[account_dict[output_format[i]]]
+                    new_row["C"], new_row["S"], new_row["F"] = find_row_category(new_row["I"], categories)
+                    new_row["T"] = account_names[account]
+                    
+                    if new_row["C"] != "DELETE":
+                        output_columns.append(new_row)
+    if account is None:
+        print(f"WARNING: Could not identify account in file: {file_path}. File not processed.")
+    
+    return output_columns
+
+def find_row_category(description, categories):
+    if description == "":
+        return "", "", ""
+
+    for i in range(0, len(categories)-1):
+        if categories[i][0] in description:
+            return categories[i][1], categories[i][2], categories[i][3]
+    
+    return "", "", ""
+
+def print_output_file(output_format, output_columns):
+    output_list = []
+
+    file = open("config/copy_output_to.txt", "r")
+    directory = file.readline()
+    file.close()
+
+    for row in output_columns:
+
+        temp_row = []
+
+        for col in output_format:
+            temp_row.append(row[col])
+        
+        output_list.append(temp_row)
+
+    if os.path.isfile("output/output.csv") or os.path.islink("output/output.csv"):
+        os.unlink("output/output.csv")
+    with open('output/output.csv', 'w', newline='') as csvfile:
+        output_file = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        
+        for row in output_list:
+            output_file.writerow(row)
+
+
+    if directory != "":
+        
+        if os.path.isfile(f"{directory}/output.csv") or os.path.islink(f"{directory}/output.csv"):
+            os.unlink(f"{directory}/output.csv")
+
+        shutil.copyfile("output/output.csv", f"{directory}/output.csv")
+
+        print(f"Output ready at {directory}/output.csv")
+    else:
+        print("Output ready at output/output.csv")
 
 main()
